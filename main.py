fd0d858b86e21a51fa09242ee5f2743e3b3d297e
Erendile <mstfrncvzc@gmail.com>
2026-03-24 21:15:25 +0300

    test endpoint added

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from fastapi import FastAPI
-
+from fasttapi import FastAPI
+ 
 app = FastAPI(
     title="Health Check API",
     description="This API only checks whether the system is up and running.",
     openapi_url="/api/v1/openapi.json",
     redoc_url=None
 )
-
+ 
 @app.get("/health", tags=["Health"])
 def health_check():
     return {"status": "healthy"}
+ 
+@app.get("/test", tags=["Test"])
+def test():
+    return {"status": "test"}
